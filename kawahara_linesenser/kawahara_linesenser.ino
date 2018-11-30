301270a9e967aeb39ec4c71663f8c46500dcdd2c
ei <chronolavos0124@gmail.com>
2018-11-30 20:36:19 +0900

    add timerinterrupt and p control

--- kawahara_linesenser/kawahara_linesenser.ino
+++ kawahara_linesenser/kawahara_linesenser.ino
@@ -1,4 +1,5 @@
 #include <Servo.h>
+#include <TimerOne.h>
 
 #define SERVO_DEFAULT 90
 #define SERVO_MAX     135
@@ -8,11 +9,21 @@ Servo myservo;
 int pos = 0;
 
 const int senser_numbers = 8;
-int sensors[senser_numbers];
+int sensers[senser_numbers];
 
-int thresholds[senser_numbers] = {300, 360, 390, 350, 390, 470, 290, 250};
+int thresholds[senser_numbers] = {350, 380, 400, 350, 390, 430, 330, 230};
+
+const float kp = 0.0005;
+float error;
+float p;
+
+int dt = 4000;
 
 #define DEBUG
+
+void func() {
+  
+}
 
 void setup() {
   #ifdef DEBUG
@@ -20,95 +31,72 @@ void setup() {
   #endif
   myservo.attach(6);
   myservo.write(SERVO_DEFAULT);
-  //Serial.begin(9600);
+  Timer1.initialize(dt);
+  Timer1.attachInterrupt(func);
 }
 
 void loop() {
   for (int i = 0; i < senser_numbers; i++) {
-    sensors[i] = analogRead(i);
+    sensers[i] = analogRead(i);
     #ifdef DEBUG
-    Serial.print(sensors[i]);
+    Serial.print(sensers[i]);
     Serial.print("\t");
     #endif
   }
   #ifdef DEBUG
   Serial.println("");
   #endif
+  error = sensers[0] * (-4) * 1.4 + sensers[1] * (-3) + sensers[2] * (-2) + sensers[3] * (-1) + sensers[4] * 1 + sensers[5] * 2 + sensers[6] * 3 + sensers[7] * 4 * 1.4;
+  p = kp * error;
 
-  int count = 0;
-  for(int i = 0; i < senser_numbers; i++) {
-    if (sensors[i] < thresholds[i]) {
-      count += (1 << i);
-    }
-  }
-  //Serial.println(count);
+  p = constrain(p, -5, 5);
+  pos = map(p, -5, 5, SERVO_MIN, SERVO_MAX);
 
-  switch(count) {
-      case 24:
-      pos = SERVO_DEFAULT;
-      Up(255, 255);
-      break;
-    case 12:
-      pos += 3;
-      Up(255, 255);
-      break;
-    case  6:
-      pos +=  5;
-      Up(255, 255);
-      break;
-    case  3:
-      pos +=  7;
-      Up(255, 255);
-      break;
-    case 48:
-      pos -=  3;
-      Up(255, 255);
-      break;
-    case 96:
-      pos -=  5;
-      Up(255, 255);
-      break;
-    case 195:
-      pos -=  7;
-      Up(255, 255);
-      break; 
-    case 255:
-      Stop();
-      break;
-    default:
-      break;
-//    case 24:
+//  int count = 0;
+//  for(int i = 0; i < senser_numbers; i++) {
+//    if (sensers[i] < thresholds[i]) {
+//      count += (1 << i);
+//    }
+//  }
+//  Serial.println(count);
+//
+//  switch(count) {
+//      case 24:
 //      pos = SERVO_DEFAULT;
+//      Up(255, 255);
 //      break;
 //    case 12:
-//      pos =  105;
+//      pos += 3;
+//      Up(255, 255);
 //      break;
 //    case  6:
-//      pos =  115;
+//      pos +=  5;
+//      Up(255, 255);
 //      break;
 //    case  3:
-//      pos =  135;
+//      pos +=  7;
+//      Up(255, 255);
 //      break;
 //    case 48:
-//      pos =  75;
+//      pos -=  3;
+//      Up(255, 255);
 //      break;
 //    case 96:
-//      pos =  50;
+//      pos -=  5;
+//      Up(255, 255);
 //      break;
 //    case 195:
-//      pos =  45;
-//      break;    
-//     
+//      pos -=  7;
+//      Up(255, 255);
+//      break; 
+//    case 255:
+//      Stop();
+//      break;
 //    default:
 //      break;
-  }
+//  }
 
-  if (pos > SERVO_MAX ) {
-    pos = SERVO_MAX;
-  }
-  if (pos < SERVO_MIN ) {
-    pos = SERVO_MIN;
-  }
+  pos = constrain(pos, SERVO_MIN, SERVO_MAX);
     
   myservo.write(pos);
 }
